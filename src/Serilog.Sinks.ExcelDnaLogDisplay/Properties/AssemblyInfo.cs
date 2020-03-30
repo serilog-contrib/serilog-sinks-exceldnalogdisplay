469e805fa30f93c56aa1df1ad021b19478518092
Caio Proiete <caio.proiete@gmail.com>
2020-03-29 21:14:53 -0300

    Strong-name sign all assemblies https://github.com/Excel-DNA/ExcelDna.StrongName

--- src/Serilog.Sinks.ExcelDnaLogDisplay/Properties/AssemblyInfo.cs
+++ src/Serilog.Sinks.ExcelDnaLogDisplay/Properties/AssemblyInfo.cs
@@ -42,4 +42,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.1.0")]
 
-[assembly: InternalsVisibleTo("Serilog.Sinks.ExcelDnaLogDisplay.Tests")]
+[assembly: InternalsVisibleTo("Serilog.Sinks.ExcelDnaLogDisplay.Tests, PublicKey=" +
+                              "00240000048000009400000006020000002400005253413100040000010001005db330d3ef1083" +
+                              "1fe51df3809c8e717ae5658de73f3a51dd72d7a7b30b49344818c2bc55fde0bfb017f907e7af2b" +
+                              "2f507e08707800dca8341ca83722cc79503a5e8449132fce7d81bfa1302fb7f000cd58837ae337" +
+                              "b00b9940ec3e433a78c2f04f816843a772f098b667b42e3df91aae44f17b8574892f49576a256b" +
+                              "bb13bcd5")]
